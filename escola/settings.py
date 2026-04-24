686c7bc247d02dbbeec555f3f86e79ce5be4fc4b
DORIVAL ALVES DA SILVA JUNIOR <dorival.junior5@senaisp.edu.br>
2026-04-24 16:06:18 -0300

    first

--- escola/settings.py
+++ escola/settings.py
@@ -75,11 +75,9 @@ WSGI_APPLICATION = 'escola.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='')
 }
 
 
